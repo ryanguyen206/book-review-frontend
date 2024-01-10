db7d3c463098b114e7124630cbcdf690525d29c9
ryanguyen206 <ryanninja595@gmail.com>
2024-01-10 12:04:03 -0800

    change comment ui

--- src/components/ReviewPage/SingleReviewFeed.jsx
+++ src/components/ReviewPage/SingleReviewFeed.jsx
@@ -19,10 +19,9 @@ const SingleReviewFeed = ({id}) => {
                 <div className='b'>
                     {comments.map(message => (
                         <div key={message.id} className='p-10 my-7 bg-gray-100 text-black'>
-                            <div className='md:flex md:justify-between'>
-                                <small>{timeAgo(message.created)}</small>  
-                                <p className='text-indigo-400'>@{message?.username} says:</p>
-                                                        
+                            <div className='flex flex-col md:flex-row md:justify-between'>
+                                <p className='order-2 md:order-1 text-indigo-400'>@{message?.username} says:</p> 
+                                <small className='order-1 md:order-2'>{timeAgo(message.created)}</small>           
                             </div>
                      
                             <p className='mt-2 tracking-tighter'>{message.body}</p>
