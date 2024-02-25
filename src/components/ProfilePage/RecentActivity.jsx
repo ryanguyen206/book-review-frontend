00362cce10c6456a68791b06b86834203d8522c3
ryanguyen206 <ryanninja595@gmail.com>
2024-02-25 13:49:11 -0800

    Show message if no comments

--- src/components/ProfilePage/RecentActivity.jsx
+++ src/components/ProfilePage/RecentActivity.jsx
@@ -27,23 +27,27 @@ const RecentActivity = ({user}) => {
         <div className='mt-10 md:col-span-2'>
         <h2 className='text-xl mt-10 mb-4 text-center md:text-start'>Recent Activity</h2>
             <div className='mt-5'>
-                {isFetching ?        
-                    <div className='mt-28'>
-                        <ReviewSkeleton /> 
-                        <br/>
-                        <ReviewSkeleton /> 
-                        <br/>
-                        <ReviewSkeleton /> 
-                    </div> 
-                : 
-                myComments && myComments.length > 0 &&
-                    myComments?.map(oneComment => (
-                        <div className='mt-5' key={oneComment.id}>
-                            <small>{timeAgo(oneComment.created)}</small>
-                            <p className=''><span className='font-semibold'>{oneComment.username} </span>said '{oneComment.body}' on <i>{oneComment.book_review.book_title} </i></p>
-                            <hr className='mt-4'/>
-                        </div>
-                    ))}
+            {isFetching ?        
+                <div className='mt-28'>
+                    <ReviewSkeleton /> 
+                    <br/>
+                    <ReviewSkeleton /> 
+                    <br/>
+                    <ReviewSkeleton /> 
+                </div> 
+            : 
+            (myComments && myComments.length > 0) ? 
+                (myComments?.map(oneComment => (
+                    <div className='mt-5' key={oneComment.id}>
+                        <small>{timeAgo(oneComment.created)}</small>
+                        <p className=''><span className='font-semibold'>{oneComment.username} </span>said '{oneComment.body}' on <i>{oneComment.book_review.book_title} </i></p>
+                        <hr className='mt-4'/>
+                    </div>
+                ))) 
+            : 
+                <p>No one has commented anything yet! &#128531;</p>
+            }
+
             </div>
         </div>
   )
